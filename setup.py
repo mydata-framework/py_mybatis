c7b7375d6ae2bb212ef7043492574cd1bbf852ab
liutao <2540006753@qq.com>
2023-08-07 22:06:22 +0800

    安装准备

---
+++ setup.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*_
+# Author: ***<***gmail.com>
+from distutils.core import setup
+
+import setuptools
+
+setup(
+
+    name='py_mybatis',  # 包的名字
+    version='0.0.1',  # 版本号
+    description='my_mybatis',  # 描述
+    author='liutao',  # 作者
+    author_email='msliutao@foxmail.com',  # 你的邮箱**
+    url='https://github.com/mydata-framework/py_mybatis',  # 可以写github上的地址，或者其他地址
+    packages=setuptools.find_packages(exclude=['test', 'examples', 'script', 'tutorials']),  # 包内不需要引用的文件夹
+
+    data_files=[('', ['*'])],
+
+    # 依赖包
+    install_requires=[
+        'JPype1==1.4.1'
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',  # Beta 阶段
+        'Operating System :: Microsoft :: Windows',  # 你的操作系统
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+        'Intended Audience :: Developers',  # Developers 是一个常见的项目受众分类标签，用于指示项目的目标受众是开发人员。
+        'License :: OSI Approved :: Apache Software License',  # BSD认证
+        'Programming Language :: Python',  # 支持的语言
+        'Programming Language :: Python :: 3',  # python版本 。。。
+        'Topic :: Software Development :: Libraries'
+    ],
+
+    zip_safe=True,
+)
+
+## 在setup.py目录下执行
+## python  setup.py  sdist
+## 最终生成一个dist文件夹，在文件夹里面就有一个创建好的安装包，格式为xxx.tar.gz的压缩包
+
+
+## pip install xxx.tar.gz
+## pip list   # 显示所有已安装的包
